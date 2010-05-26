acd28a058f68cb40031e19414e84536d0497226f
Sam Steingold <sds@gnu.org>
2010-05-26 18:46:12 +0000

    hg-162f74e0e8f1 comment
    git-svn-id: svn+ssh://svn.forge.ocamlcore.org/svn/tuareg@48 a37b5e43-4b7f-4128-9b7a-df1949a3dd69

--- tuareg.el
+++ tuareg.el
@@ -391,8 +391,7 @@ and `tuareg-xemacs-w3-manual' (XEmacs only)."
 ;; (make-variable-buffer-local 'tuareg-interactive-program)
 
 ;; Backtrack to custom parsing and caching by default, until stable
-;;(defvar tuareg-use-syntax-ppss (fboundp 'syntax-ppss)
-(defconst tuareg-use-syntax-ppss nil
+(defconst tuareg-use-syntax-ppss nil ; (fboundp 'syntax-ppss)
   "*If nil, use our own parsing and caching.")
 
 (defgroup tuareg-faces nil
