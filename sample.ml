8844bd906ea5eebd1a168f470a2a595e4dfdefc1
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2012-09-29 16:20:26 +0000

    Issue: align arguments with a large braced one.
    git-svn-id: svn+ssh://svn.forge.ocamlcore.org/svn/tuareg/trunk@456 a37b5e43-4b7f-4128-9b7a-df1949a3dd69

--- sample.ml
+++ sample.ml
@@ -1167,3 +1167,12 @@ val f :
   int ->
   int
 
+let subscribe_impl dir topic ~aborted =
+  return (
+      match Directory.subscribe dir topic with
+      | None -> Error ()
+      | Some pipe ->
+         whenever (aborted >>| fun () -> Pipe.close_read pipe);
+         Ok pipe
+    )
+    next_argument (* should be indented correctly, given the braces *)
