b90e4d3d5cf67ddc4ffd47143f9abb5733508c5c
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2013-08-15 12:36:07 +0000

    Specify explicitly the governing face on TTY.
    git-svn-id: svn+ssh://svn.forge.ocamlcore.org/svn/tuareg/trunk@464 a37b5e43-4b7f-4128-9b7a-df1949a3dd69

--- tuareg.el
+++ tuareg.el
@@ -496,7 +496,8 @@ Valid names are `browse-url', `browse-url-firefox', etc."
        (assq :inherit face-attribute-name-alist)))
 
 (defface tuareg-font-lock-governing-face
-  '((((background light)) (:foreground "black" :bold t))
+  '((((class color) (type tty)) (:bold t))
+    (((background light)) (:foreground "black" :bold t))
     (t (:foreground "wheat" :bold t)))
   "Face description for governing/leading keywords."
   :group 'tuareg-faces)
