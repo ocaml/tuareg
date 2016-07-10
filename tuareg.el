1e68a0addca6165b8f1cadfcd15c0b7ef5184e9a
Stefan Monnier <monnier@iro.umontreal.ca>
2016-07-10 13:45:29 -0400

    * tuareg.el (tuareg-syntax-propertize): Pay attention to tuareg--syntax-quotation's final position (issue#89). (tuareg--syntax-quotation): Always move point to end of quotation.

--- tuareg.el
+++ tuareg.el
@@ -645,7 +645,7 @@ Regexp match data 0 points to the chars."
       ("\\({\\)[a-z_]*|"
        (1 (prog1 "|" (tuareg--syntax-quotation end))))
       )
-     start end)))
+     (point) end)))
 
 (defun tuareg--syntax-quotation (end)
   (let ((ppss (syntax-ppss)))
@@ -653,7 +653,7 @@ Regexp match data 0 points to the chars."
       (ecase (char-after (nth 8 ppss))
         (?<
          ;; We're indeed inside a quotation.
-         (when (re-search-forward ">>" end t)
+         (when (re-search-forward ">>" end 'move)
            (put-text-property (1- (point)) (point)
                               'syntax-table (string-to-syntax "|"))))
         (?\{
@@ -664,7 +664,7 @@ Regexp match data 0 points to the chars."
                      (buffer-substring (point)
                                        (progn (skip-chars-forward "a-z_")
                                               (point))))))
-           (when (search-forward (concat "|" id "}") end t)
+	   (when (search-forward (concat "|" id "}") end 'move)
              (put-text-property (1- (point)) (point)
                                 'syntax-table (string-to-syntax "|")))))))))
 
