031bcd39c38a735d0b2498c8a2e54c8e57531c3a
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2017-09-10 20:26:26 +0200

    Remove references to tuareg_indent.el

--- tuareg.el
+++ tuareg.el
@@ -25,7 +25,7 @@
 
 ;; Installation:
 ;; If you have permissions to the local `site-lisp' directory, you
-;; only have to copy `tuareg.el', `tuareg_indent.el', `ocamldebug.el'
+;; only have to copy `tuareg.el', `ocamldebug.el'
 ;; and `tuareg-site-file.el'.  Otherwise, copy the previous files
 ;; to a local directory and add the following line to your `.emacs':
 ;;
