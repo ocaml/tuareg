f929e34c327e25c783995e3c1d72c5153c779468
Mattias Engdegård <mattiase@acm.org>
2021-08-08 15:09:53 +0200

    Skip ;; even on a separate line (fixes #259)
    This allows repeated `tuareg-eval-phrase` to execute phrases in
    sequence without the user needing to do anything in-between.

--- tuareg.el
+++ tuareg.el
@@ -2532,7 +2532,8 @@ Return a non-nil value if a comment was skipped."
 
 (defun tuareg--skip-forward-comments-semicolon ()
   "Skip ';;' and then 'sticky' comments after a definition."
-  (skip-chars-forward " \t;")
+  (when (looking-at (rx (* (in " \t\n")) ";;"))
+    (goto-char (match-end 0)))
   (while (tuareg--skip-forward-comment)))
 
 (defvar-local tuareg-smie--forward-and-cache nil
