f6801c86e921949a4a1bb9088bf85b36a9cd890d
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2021-08-25 10:58:19 +0200

    Update Tuareg header

--- tuareg.el
+++ tuareg.el
@@ -21,12 +21,12 @@
 ;; EmacsWiki: TuaregMode
 
 ;;; Commentary:
-;; Description:
 ;; Tuareg helps editing OCaml code, to highlight important parts of
 ;; the code, to run an OCaml REPL, and to run the OCaml debugger
 ;; within Emacs.
+;; See https://github.com/ocaml/tuareg for customization tips.
 
-;; Installation:
+;;; Installation:
 ;; If you have permissions to the local `site-lisp' directory, you
 ;; only have to copy `tuareg.el', `ocamldebug.el'
 ;; and `tuareg-site-file.el'.  Otherwise, copy the previous files
